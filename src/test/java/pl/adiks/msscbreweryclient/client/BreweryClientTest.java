79f7b6924e25cffea76159bbdb2f7a0cd0105a96
Adiks <apacholak97@interia.pl>
2020-10-14 19:39:29 +0200

    Integration test

---
+++ src/test/java/pl/adiks/msscbreweryclient/client/BreweryClientTest.java
@@ -0,0 +1,25 @@
+package pl.adiks.msscbreweryclient.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import pl.adiks.msscbreweryclient.model.BeerDTO;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BreweryClientTest {
+
+    @Autowired
+    BreweryClient breweryClient;
+
+    @Test
+    void getBeerById() {
+
+        BeerDTO beerDTO = breweryClient.getBeerById(UUID.randomUUID());
+
+        assertNotNull(beerDTO);
+    }
+}
