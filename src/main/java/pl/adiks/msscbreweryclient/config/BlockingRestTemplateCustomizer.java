293a850ee61a90cc5c815d223eb89fb5af407995
Adiks <apacholak97@interia.pl>
2020-10-16 20:00:21 +0200

    assignment

--- src/main/java/pl/adiks/msscbreweryclient/config/BlockingRestTemplateCustomizer.java
+++ src/main/java/pl/adiks/msscbreweryclient/config/BlockingRestTemplateCustomizer.java
@@ -5,24 +5,36 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateCustomizer;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Component
+@PropertySource("classpath:application.properties")
 public class BlockingRestTemplateCustomizer implements RestTemplateCustomizer {
+
+    @Value("${block.connection.max.total}")
+    private  Integer maxTotal;
+
+    @Value("${block.connection.max.per.route}")
+    private  Integer maxPerRoute;
+
+    @Value("${block.connection.timeout}")
+    private  Integer timeout;
 
     public ClientHttpRequestFactory clientHttpRequestFactory(){
         PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
-        connectionManager.setMaxTotal(100);
-        connectionManager.setDefaultMaxPerRoute(20);
+        connectionManager.setMaxTotal(maxTotal);
+        connectionManager.setDefaultMaxPerRoute(maxPerRoute);
 
         RequestConfig requestConfig = RequestConfig
                 .custom()
-                .setConnectionRequestTimeout(3000)
-                .setSocketTimeout(3000)
+                .setConnectionRequestTimeout(timeout)
+                .setSocketTimeout(timeout)
                 .build();
 
         CloseableHttpClient httpClient = HttpClients
